ab62957c6b41a0d1245b4a87953250065991160d
Anton Adamansky <adamansky@gmail.com>
2022-08-30 22:20:05 +0700

    *

--- src/aws4.c
+++ src/aws4.c
@@ -1,18 +1,12 @@
 #include "aws4.h"
 #include "config.h"
 
-#include <iowow/iwxstr.h>
 #include <iowow/iwlog.h>
-#include <iowow/iwp.h>
-#include <iowow/iwutils.h>
-#include <iowow/iwini.h>
+#include <iwnet/iwn_curl.h>
 
 #include <errno.h>
-#include <getopt.h>
-#include <limits.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
+
+
--- src/aws4.h
+++ src/aws4.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#include <iowow/iwpool.h>
+#include <iowow/basedefs.h>
 
