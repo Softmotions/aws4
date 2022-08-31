e4783fa09c33a9ad3954d19ad02cf638d5f8d078
Anton Adamansky <adamansky@gmail.com>
2022-08-31 12:35:12 +0700

    *

--- src/aws4.c
+++ src/aws4.c
@@ -2,7 +2,7 @@
 #include "config.h"
 
 #include <iowow/iwlog.h>
-#include <iwnet/iwn_curl.h>
+#include <iwnet/iwn_codec.h>
 
 #include <errno.h>
 #include <stdio.h>
--- src/aws4.h
+++ src/aws4.h
@@ -1,4 +1,25 @@
 #pragma once
 
 #include <iowow/basedefs.h>
+#include <iwnet/iwn_curl.h>
+
+
+struct aws4_request_sign_spec {
+
+};
+
+
+iwrc aws4_request_sign(const struct aws4_request_sign_spec *spec, struct xcurlreq *req);
+
+
+
+
+
+
+
+
+
+
+
+
 
