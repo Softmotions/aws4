ed0daf1b9b7eefd15e0accf3fccd605e14c179c5
Anton Adamansky <adamansky@gmail.com>
2022-09-06 18:37:21 +0700

    *

--- src/aws4.c
+++ src/aws4.c
@@ -73,12 +73,88 @@ static iwrc _cr_uri_add(struct xcurlreq *req, IWXSTR *xstr) {
   return iwxstr_cat(xstr, "\n", 1);
 }
 
+static int _cr_qs_pair_compare(const void *a, const void *b) {
+  const struct iwn_pair *p1 = a;
+  const struct iwn_pair *p2 = b;
+  int ret = p1->key_len > p2->key_len ? 1 : p1->key_len < p2->key_len ? -1 : 0;
+  if (ret) {
+    return ret;
+  }
+  ret = strncmp(p1->key, p2->key, p1->key_len);
+  if (ret) {
+    return ret;
+  }
+  ret = p1->val_len > p2->val_len ? 1 : p1->val_len < p2->val_len ? -1 : 0;
+  if (ret) {
+    return ret;
+  }
+  return strncmp(p1->val, p2->val, p1->val_len);
+}
+
 static iwrc _cr_qs_add(struct xcurlreq *req, IWXSTR *xstr) {
   if (!req->_qxstr || iwxstr_size(req->_qxstr) == 0) {
     return iwxstr_cat(xstr, "\n", 1);
   }
   iwrc rc = 0;
-  
+  IWPOOL *pool = 0;
+  struct iwn_pairs pairs;
+
+  char *buf = 0;
+  size_t buflen = 0;
+
+  size_t len = iwxstr_size(req->_qxstr);
+  RCB(finish, pool = iwpool_create_empty());
+
+  char *qs = iwpool_strndup2(pool, iwxstr_ptr(req->_qxstr), len);
+  RCB(finish, qs);
+  RCC(rc, finish, iwn_wf_parse_query_inplace(pool, &pairs, qs, len));
+
+  struct iwn_pair **parr = iwn_pairs_to_array(pool, &pairs, &len);
+  RCB(finish, parr);
+
+  for (size_t i = 0; i < len; ++i) {
+    struct iwn_pair *p = parr[i];
+    iwn_url_decode_inplace2((char*) p->key, (char*) p->key + p->key_len);
+    iwn_url_decode_inplace2(p->val, p->val + p->val_len);
+  }
+
+  // NOLINTNEXTLINE
+  qsort(parr, len, sizeof(parr[0]), _cr_qs_pair_compare);
+
+  for (size_t i = 0; i < len; ++i) {
+    struct iwn_pair *p = parr[i];
+    len = iwn_url_encoded_len(p->key, p->key_len);
+    if (len > buflen) {
+      RCB(finish, buf = realloc(buf, len));
+      buflen = len;
+    }
+    if (i) {
+      RCC(rc, finish, iwxstr_cat(xstr, "&", 1));
+    }
+    iwn_url_encode(p->key, p->key_len, buf, buflen);
+    RCC(rc, finish, iwxstr_cat(xstr, buf, len));
+    RCC(rc, finish, iwxstr_cat(xstr, "=", 1));
+    if (p->val_len) {
+      len = iwn_url_encoded_aws_len(p->val, p->val_len);
+      if (len > buflen) {
+        RCB(finish, buf = realloc(buf, len));
+        buflen = len;
+      }
+      iwn_url_encode_aws(p->val, p->val_len, buf, buflen);
+      RCC(rc, finish, iwxstr_cat(xstr, buf, len));
+    }
+  }
+
+  RCC(rc, finish, iwxstr_cat(xstr, "\n", 1));
+
+finish:
+  free(buf);
+  iwpool_destroy(pool);
+  return rc;
+}
+
+static iwrc _cr_headers_add(struct xcurlreq *req, IWXSTR *xstr) {
+  iwrc rc = 0;
 
 
   return rc;
@@ -92,7 +168,8 @@ iwrc aws4_request_sign(const struct aws4_request_sign_spec *spec, struct xcurlre
   }
   RCC(rc, finish, _cr_method_add(req, cr));
   RCC(rc, finish, _cr_uri_add(req, cr));
-
+  RCC(rc, finish, _cr_qs_add(req, cr));
+  RCC(rc, finish, _cr_headers_add(req, cr));
 
 finish:
   iwxstr_destroy(cr);
