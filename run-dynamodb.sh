81acf642cdecce059441a04356b17b87d66ddcac
Anton Adamansky <adamansky@gmail.com>
2022-11-24 00:30:07 +0200

    *

--- run-dynamodb.sh
+++ run-dynamodb.sh
@@ -7,5 +7,5 @@ if [ -z $DYNAMODB_HOME ]; then
 fi
 
 echo "Dynamodb home: ${DYNAMODB_HOME}"
-trap 'pkill -P $$' EXIT
+trap "kill 0" EXIT
 java -Djava.library.path=${DYNAMODB_HOME}/DynamoDBLocal_lib -jar ${DYNAMODB_HOME}/DynamoDBLocal.jar -sharedDb $@
