6c6319e4af27cdcb1e626d14b11f273ddce58ba2
Anton Adamansky <adamansky@gmail.com>
2023-01-22 01:51:08 +0200

    * cmake scripts refactoring

--- cmake/Modules/AddEJDB2.cmake
+++ cmake/Modules/AddEJDB2.cmake
@@ -4,7 +4,7 @@ if(TARGET EJDB2::static)
   return()
 endif()
 
-set(EJDB2_INCLUDE_DIRS "")
+set(EJDB2_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/include)
 
 include(ExternalProject)
 include(AddIWNET)
@@ -23,15 +23,10 @@ set(CMAKE_ARGS
     -DBUILD_SHARED_LIBS=OFF
     -DBUILD_EXAMPLES=OFF)
 
-set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} ${CMAKE_INSTALL_PREFIX})
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} ${CMAKE_INSTALL_PREFIX}
+                         ${CMAKE_BINARY_DIR})
 
-# In order to properly pass owner project CMAKE variables than contains
-# semicolons, we used a specific separator for 'ExternalProject_Add', using the
-# LIST_SEPARATOR parameter. This allows building fat binaries on macOS, etc.
-# (ie: -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64") Otherwise, semicolons get
-# replaced with spaces and CMake isn't aware of a multi-arch setup.
-set(SSUB "^^")
-
+set(SSUB "|")
 foreach(
   extra
   ANDROID_ABI
@@ -48,8 +43,6 @@ foreach(
   PLATFORM
   TEST_TOOL_CMD)
   if(DEFINED ${extra})
-    # Replace occurences of ";" with our custom separator and append to
-    # CMAKE_ARGS
     string(REPLACE ";" "${SSUB}" extra_sub "${${extra}}")
     list(APPEND CMAKE_ARGS "-D${extra}=${extra_sub}")
   endif()
@@ -66,8 +59,8 @@ ExternalProject_Add(
   BUILD_IN_SOURCE OFF
   # DOWNLOAD_EXTRACT_TIMESTAMP ON
   UPDATE_COMMAND ""
-  LIST_SEPARATOR "${SSUB}"
   CMAKE_ARGS ${CMAKE_ARGS}
+  LIST_SEPARATOR "${SSUB}"
   BUILD_BYPRODUCTS ${BYPRODUCT})
 
 add_library(EJDB2::static STATIC IMPORTED GLOBAL)
@@ -75,7 +68,7 @@ set_target_properties(
   EJDB2::static
   PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES "C"
              IMPORTED_LOCATION
-             "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/libejdb2-2.a"
+             ${BYPRODUCT}
              IMPORTED_LINK_INTERFACE_LIBRARIES "IWNET::static")
 
 add_dependencies(EJDB2::static extern_ejdb2)
