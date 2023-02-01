3a260b686bdb7d5c417cd4615eab43fdcf4edf63
Anton Adamansky <adamansky@gmail.com>
2023-02-01 21:32:48 +0200

    * minors

--- src/tests/run-dynamodb.sh
+++ src/tests/run-dynamodb.sh
@@ -8,4 +8,4 @@ fi
 
 echo "Dynamodb home: ${DYNAMODB_HOME}"
 trap 'pkill -P $$' EXIT
-java -Djava.library.path=${DYNAMODB_HOME}/DynamoDBLocal_lib -jar ${DYNAMODB_HOME}/DynamoDBLocal.jar -sharedDb --inMemory $@
+java -Djava.library.path=${DYNAMODB_HOME}/DynamoDBLocal_lib -jar ${DYNAMODB_HOME}/DynamoDBLocal.jar -inMemory $@
